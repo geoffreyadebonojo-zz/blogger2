f04079f59b10560c3ab020151d8fc7d7c73e4ae4
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-10-20 13:02:42 -0600

    add styling and links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,9 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+    @time = @article.created_at.rfc2822[0..25]
+  end
 end
